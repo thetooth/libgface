b32907aee098b750b916cdf5b6bd439f611dd8cc
Jeff Jenner <thetooth@ameoto.com>
2012-05-05 22:53:16 +1000

    Implemented gface license code

--- src/gface/gface.c
+++ src/gface/gface.c
@@ -1,10 +1,23 @@
 #include "gface.h"
 
 static bool validated = false;
+static char* keys[32] = {
+	"ED65D668-CE4F-4A28-99CD-FD7670C558C4",
+	"5B057545-9213-4AD0-9D67-AD01FFD25C0F",
+	"17ABBA43-5020-4E95-A889-84E408DB0BAA",
+	"42C66D9B-9722-4D4B-9F39-3D1E43EFC96B",
+	"274BD4D1-0651-4047-B249-6A97D33A4A8A"
+};
 
 bool gface_init(char *license_key)
 {
-    // TODO: implement gface license key checking
+	int i;
+	for (i = 0; i <= 5; i++)
+	{
+		if (strncmp(license_key, keys[i], 32) == 0){
+			return true;
+		}
+	}
     return false;
 }
 
@@ -13,7 +26,7 @@ wchar_t *gface_acquire()
     if(validated) {
         return L"( ≖‿≖)";
     }
-    return L"Your gface license is inactive or otherwhise invalid. If you believe this is in error, contact licensing@xn--hwg34fba.ws.";
+    return L"Your gface license is inactive or otherwise invalid. If you believe this is in error, contact licensing@xn--hwg34fba.ws.";
 }
 
 void gface_shutdown()
--- src/gface/gface.h
+++ src/gface/gface.h
@@ -2,9 +2,12 @@
 #define false 0
 #define true 1
 #define bool int
+#define VC_EXTRALEAN
+#define WIN32_LEAN_AND_MEAN
 #else
 #include <stdbool.h>
 #endif
+#include <string.h>
 #include <wchar.h>
 
 #if !defined(_GFACE_GFACE_H)
